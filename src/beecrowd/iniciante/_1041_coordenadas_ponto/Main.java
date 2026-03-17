459e1aab0937b011f872aa0bbf2ae68d01283bb3
Kaike Aniceto <kaikeaniceto@gmail.com>
2026-03-17 15:10:04 -0300

    Exercicio 1041: Coordenadas de um ponto

---
+++ src/beecrowd/iniciante/_1041_coordenadas_ponto/Main.java
@@ -0,0 +1,35 @@
+package beecrowd.iniciante._1041_coordenadas_ponto;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        float x = scanner.nextFloat();
+        float y = scanner.nextFloat();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Q4");
+        } else if (x == 0) {
+            System.out.println("Eixo Y");
+        } else if (y == 0) {
+            System.out.println("Eixo X");
+        }
+
+        scanner.close();
+    }
+}
