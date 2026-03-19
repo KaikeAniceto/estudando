b1b8ee058d4dbca5e055620cb47118aa24638df0
Kaike Aniceto <kaikeaniceto@gmail.com>
2026-03-19 16:26:25 -0300

    Exercicio 1044: Multiplo

---
+++ src/beecrowd/iniciante/_1044_multiplo/Main.java
@@ -0,0 +1,21 @@
+    package beecrowd.iniciante._1044_multiplo;
+    import java.io.IOException;
+    import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+
+            Scanner scanner = new Scanner(System.in);
+
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            if ((A % B == 0) || (B % A == 0)) {
+                System.out.println("Sao Multiplos");
+            } else {
+                System.out.println("Nao sao Multiplos");
+            }
+            scanner.close();
+        }
+    }
