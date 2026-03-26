d026b338a1c5f01c7b3a88d2350ec54f452ae4f1
Kaike Aniceto <kaikeaniceto@gmail.com>
2026-03-25 23:47:08 -0300

    Exercicio 1046: Tempo de Jogo

--- src/beecrowd/iniciante/_1046_tempo_jogo/Main.java
+++ src/beecrowd/iniciante/_1046_tempo_jogo/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-
         int hora_inicio = scanner.nextInt();
         int hora_fim = scanner.nextInt();
         int duracao;
